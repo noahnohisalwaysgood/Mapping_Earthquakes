a10bd9b362c52fb0d5b31a84cd4aed3efccccf9a
Noahnohisalwyasgood <105985796+Noahnohisalwyasgood@users.noreply.github.com>
2022-09-01 22:27:50 -0400

    09012022

--- Mapping_Single_Points/js/logic.js
+++ Mapping_Single_Points/js/logic.js
@@ -4,11 +4,23 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// We create the tile layer that will be the background of our map.
+// Create the map object with a center and zoom level.
+L.circle([34.0522, -118.2437], {
+    radius: 100
+ }).addTo(map);
+
+// add a circle to the map
+L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color: "black",
+    fillColor: '#ffffa1'
+ }).addTo(map);
+
+ // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
